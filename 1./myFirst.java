19aee26e12de1bc8ec06f5bb7750f60bed799176
Siddharth Rai <124533649+itsSidR@users.noreply.github.com>
2025-01-09 12:17:09 -0600

    Created 1st java program where you read from keyb.

---
+++ 1./myFirst.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class myFirst
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        String name;
+        System.out.println("What is your name?");
+        name = s.nextLine();
+        System.out.println("Welcome " + name);
+    }
+}
